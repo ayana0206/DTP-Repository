53f43ff818b2cfdbbd7f022131307980367e0dfe
ayana0206 <schwizerayana@gmail.com>
2025-12-07 16:41:23 +0100

    SortingAlgorithm

---
+++ SortingAlgorithm/SortingAlgorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SortingAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*IList<int> list = new List<int> { 4, 2, 7, 10, 5, 1, 5, 2, 0 };
+
+            BubbleSort sorter = new BubbleSort(list);
+
+            Console.WriteLine("BubbleSort");
+            foreach (int i in sorter.Sort())
+            {
+                Console.WriteLine(i);
+            }
+
+            Quicksort sorter2 = new Quicksort(list);
+
+            Console.WriteLine("Quicksort");
+            foreach (int i in sorter2.Sort())
+            {
+                Console.WriteLine(i);
+            }*/
+
+            int[] array = { 4, 2, 7, 10, 5, 1, 5, 2, 0 };
+
+
+            MergeSort sorter3 = new MergeSort(array);
+
+            Console.WriteLine("Mergesort\n");
+            Console.WriteLine("bevor sorting:");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n\nafter sorting:");
+
+            foreach (int i in sorter3.Sort())
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n\n\n\n");
+        }
+    }
+}
---
+++ SortingAlgorithm/SortingAlgorithm/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    internal class Quicksort
+    {
+        private IList<int> listToSort;
+
+        public Quicksort(IList<int> listToSort)
+        {
+            this.listToSort = listToSort;
+        }
+
+        public IList<int> Sort()
+        {
+            Sort(listToSort, 0, listToSort.Count - 1);
+            return listToSort;
+        }
+
+        private void Sort(IList<int> array, int start, int end)
+        {
+            if (end <= start) return;
+
+            int pivot = Partition(array, start, end);
+            Sort(array, start, pivot - 1);
+            Sort(array, pivot + 1, end);
+        }
+
+        private int Partition(IList<int> array, int start, int end)
+        {
+            int pivot = array[end];
+            int i = start - 1;
+            for (int j = start; j < end; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            i++;
+            var temp2 = array[i];
+            array[i] = array[end];
+            array[end] = temp2;
+            return i;
+        }
+    }
+}
